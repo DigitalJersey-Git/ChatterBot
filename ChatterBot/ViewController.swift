dace143a69a2d1d26c5be8ed85f63028a46d4e54
Chris Mousdale <chris.mousdale@tpagency.com>
2017-02-23 18:05:58 +0000

    Initial Commit

---
+++ ChatterBot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatterBot
+//
+//  Created by Chris Mousdale on 23/02/2017.
+//  Copyright © 2017 Chris Mousdale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
